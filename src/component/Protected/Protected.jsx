00c6dd4f7280584bb9d03a7bf5c229919c2049f2
ankur0207 <ankursharma0207@gmail.com>
2023-01-06 16:22:09 +0530

    Laoding Fixed

--- src/component/Protected/Protected.jsx
+++ src/component/Protected/Protected.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { useAuth } from '../../context/AuthContext';
 import { Navigate } from 'react-router-dom';
-
+import Loading from '../Loading/Loading';
 function Protected({ children }) {
-  const { user } = useAuth();
-  if (!user) return <Navigate to='/signin' />;
-  
-  return <>{children}</>;
+  const { user, loading } = useAuth();
+
+  return <>{loading ? <Loading /> : user ? children : <Navigate to='/signin' />}</>;
 }
 
 export default Protected;
